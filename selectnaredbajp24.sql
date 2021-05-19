4ac37ba23c93c8f63821a11356f2a51096af6282
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-05-19 09:53:08 +0100

    Spickovina

--- selectnaredbajp24.sql
+++ selectnaredbajp24.sql
@@ -115,13 +115,17 @@ select * from country where code like '%HRV%';
 
 # unesite mjesto Donji Miholjac
 select * from city;
+
+insert into city (Name, CountryCode, District, Population) 
+values ('Donji Miholjac', 'HRV', 'Osijek-Baranja', 10000);
+
 select * from city where name like '%Donji Miholjac%';
 
 # promjenite Donji Miholjac u Špičkovinu
 select * from city;
-select * from city where name like '%Donji Miholjac%';
+update city set Name='Spickovina' where ID=4080;
 
-# update city set Name='Špičkovina'
-# where ID=4080;
+select * from city where name like '%Spickovina%';
 
 # obrišite mjesto Špičkovina
+delete from city where ID=4080;
